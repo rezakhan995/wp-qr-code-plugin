ade898ea34a3909efb7c67ebf76915e1595128f9
Reza Khan <rezakhan995@gmail.com>
2020-01-18 23:40:05 +0600

    Added countris dropdown for more customization

--- posts_to_qrcode.php
+++ posts_to_qrcode.php
@@ -50,10 +50,47 @@ add_filter("the_content", "ptqrc_display_qrcode", 99);
 
 function ptqrc_show_settings()
 {
-    add_settings_field("ptqrc_height", __("QR Code Height", "qr-post"), 'ptqrc_set_height', "general");
-    add_settings_field("ptqrc_width", __("QR Code Width", "qr-post"), 'ptqrc_set_width', "general");
+
+    add_settings_section("ptqrc_settings_section", __("Post To QrCode Settings", "qr-post"), "ptqrc_settings_section", "general");
+
+    add_settings_field("ptqrc_height", __("QR Code Height", "qr-post"), 'ptqrc_set_height', "general", "ptqrc_settings_section");
+    add_settings_field("ptqrc_width", __("QR Code Width", "qr-post"), 'ptqrc_set_width', "general", "ptqrc_settings_section");
+    add_settings_field("ptqrc_country", __("Select coutries to show QR Code", "qr-post"), "ptqrc_select_country", "general", "ptqrc_settings_section");
+
+
+
+    register_setting("general", "ptqrc_country", array("sanitize_callback" => "esc_attr"));
     register_setting("general", "ptqrc_height", array("sanitize_callback" => "esc_attr"));
     register_setting("general", "ptqrc_width", array("sanitize_callback" => "esc_attr"));
+}
+
+function ptqrc_select_country()
+{
+
+    $country = get_option("ptqrc_country");
+    $countries = array(
+        "None",
+        "Bangladesh",
+        "India",
+        "Pakinstan",
+        "Myanmar",
+        "Nepal",
+        "Srilanka"
+    );
+    printf("<select name='%s' id='%s'>", "ptqrc_country", "ptqrc_country");
+    foreach ($countries as $single_country) {
+        $select = "";
+        if ($country == $single_country) {
+            $select = "selected";
+        }
+        printf("<option id='%s' %s>%s</option>", $single_country, $select, $single_country);
+    }
+    printf("</select>");
+}
+
+function ptqrc_settings_section()
+{
+    echo "<h5>This section is used to configure the height and width for QR Code</h5>";
 }
 
 function ptqrc_set_height()
